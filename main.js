e2ddbea3ee0a944d17e7afeb8ae488dfb6f5eabe
topdatop01 <topdatop01@outlook.com>
2026-08-17 12:26:14 +0300

    0.1.0-alpha

---
+++ main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+const debug = false
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1024,
+        height: 768,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+
+    if (!debug) {
+        Menu.setApplicationMenu(null);
+    }
+
+    win.loadFile('index.html');
+
+    if (debug) {
+        win.webContents.openDevTools();
+    }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+if (debug === false) {
+    win.webContents.on('devtools-opened', () => {
+        win.webContents.closeDevTools();
+    });
+}
